0abee99350426bce4c89093f62f42863a8db9ed3
Ogundele Caleb <ogundelecaleb13@gmail.com>
2024-04-10 17:41:22 +0100

    commit

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,5 +1,5 @@
 
-export const API_BASE_URL = 'https://test.lasucom.solarcredit.io/api';
+export const API_BASE_URL = 'https://lasucom.iccflifeskills.com.ng/api';
 export const getApiUrl = (endpoint) => API_BASE_URL + endpoint
 
 export const GETDASHBAORD = getApiUrl('/dashboard')
