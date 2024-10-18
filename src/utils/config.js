af2a5c3518b0350ae2814245f5570f60935201fe
caleb <ogundelecaleb13@gmail.com>
2024-10-18 15:02:35 +0100

    commit

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,5 +1,5 @@
 
-export const API_BASE_URL = 'https://hrm.devapi.live/api';
+export const API_BASE_URL = 'https://hrmdev.devapi.live/api';
 export const getApiUrl = (endpoint) => API_BASE_URL + endpoint
 
 export const GETDASHBAORD = getApiUrl('/dashboard')
