e37ab282f81d14f868b6d805e986ac4c3c3753a9
caleb <ogundelecaleb13@gmail.com>
2025-04-17 00:14:53 +0100

    updated base url

--- src/utils/AuthConfig.js
+++ src/utils/AuthConfig.js
@@ -1,6 +1,6 @@
 
-export const API_BASE_URL = 'https://hrmdev.devapi.live/api';
-//export const API_BASE_URL = 'https://hrm.devapi.live/api';
+//export const API_BASE_URL = 'https://hrmdev.devapi.live/api';
+export const API_BASE_URL = 'https://hrm.devapi.live/api';
 export const getApiUrl = (endpoint) => API_BASE_URL + endpoint
 
 export const LOGIN = getApiUrl('/login')
